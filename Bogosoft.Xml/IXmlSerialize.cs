2bb428b8894e33b10a9434ebd8e872a66e058f0c
Clayton Roth <croth0627@yahoo.com>
2016-08-30 00:32:24 -0700

    Added contract for self XML serialization.

---
+++ Bogosoft.Xml/IXmlSerialize.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml;
+
+namespace Bogosoft.Xml
+{
+    /// <summary>
+    /// Indicates that a type has the ability to serialize itself to an <see cref="XmlNode"/>.
+    /// </summary>
+    public interface IXmlSerialize
+    {
+        /// <summary>
+        /// Serialize the current instance to an <see cref="XmlNode"/>.
+        /// </summary>
+        /// <param name="target">The <see cref="XmlNode"/> to serialize to.</param>
+        void SerializeTo(XmlNode target);
+    }
+}
---
+++ Bogosoft.Xml/XmlSerializerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml;
+
+namespace Bogosoft.Xml
+{
+    /// <summary>
+    /// Extended functionality for the <see cref="IXmlSerialize"/> contract.
+    /// </summary>
+    public static class XmlSerializerExtensions
+    {
+        /// <summary>
+        /// Serialize the current instance to an <see cref="XmlDocument"/>.
+        /// </summary>
+        /// <param name="serializable">The current <see cref="IXmlSerialize"/> implementation.</param>
+        /// <returns>A serialized <see cref="XmlDocument"/>.</returns>
+        public static XmlDocument Serialize(this IXmlSerialize serializable)
+        {
+            var document = new XmlDocument();
+
+            serializable.SerializeTo(document);
+
+            return document;
+        }
+    }
+}
