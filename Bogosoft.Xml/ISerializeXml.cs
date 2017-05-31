6614270825d453190a23a217c854bc7a54e955dd
Clayton Roth <croth0627@yahoo.com>
2017-05-31 00:26:22 -0700

    renamed `IXmlSerializable` to `ISerializeXml`

--- Bogosoft.Xml/ISerializeXml.cs
+++ Bogosoft.Xml/ISerializeXml.cs
@@ -5,7 +5,7 @@ namespace Bogosoft.Xml
     /// <summary>
     /// Indicates that a type has the ability to serialize itself to an <see cref="XmlNode"/>.
     /// </summary>
-    public interface IXmlSerializable
+    public interface ISerializeXml
     {
         /// <summary>
         /// Serialize the current instance to an <see cref="XmlNode"/>.
--- Bogosoft.Xml/XmlSerializerExtensions.cs
+++ Bogosoft.Xml/XmlSerializerExtensions.cs
@@ -4,18 +4,18 @@ using System.Xml.XPath;
 namespace Bogosoft.Xml
 {
     /// <summary>
-    /// Extended functionality for the <see cref="IXmlSerializable"/> contract.
+    /// Extended functionality for the <see cref="ISerializeXml"/> contract.
     /// </summary>
     public static class XmlSerializerExtensions
     {
         /// <summary>
         /// Serialize the current instance to an <see cref="XmlDocument"/>.
         /// </summary>
-        /// <param name="serializable">The current <see cref="IXmlSerializable"/> implementation.</param>
+        /// <param name="serializable">The current <see cref="ISerializeXml"/> implementation.</param>
         /// <returns>
         /// An XPath-navigable object.
         /// </returns>
-        public static XmlDocument Serialize(this IXmlSerializable serializable)
+        public static XmlDocument Serialize(this ISerializeXml serializable)
         {
             var document = new XmlDocument();
 
