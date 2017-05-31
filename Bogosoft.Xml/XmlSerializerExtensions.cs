39d2b9fd2c271f1eb8212273d93e7c6f9214b96d
Clayton Roth <croth0627@yahoo.com>
2017-05-31 00:36:21 -0700

    reverted name change for local XML serializable interface

--- Bogosoft.Xml/XmlSerializerExtensions.cs
+++ Bogosoft.Xml/XmlSerializerExtensions.cs
@@ -4,18 +4,18 @@ using System.Xml.XPath;
 namespace Bogosoft.Xml
 {
     /// <summary>
-    /// Extended functionality for the <see cref="ISerializeXml"/> contract.
+    /// Extended functionality for the <see cref="IXmlSerializable"/> contract.
     /// </summary>
     public static class XmlSerializerExtensions
     {
         /// <summary>
         /// Serialize the current instance to an <see cref="XmlDocument"/>.
         /// </summary>
-        /// <param name="serializable">The current <see cref="ISerializeXml"/> implementation.</param>
+        /// <param name="serializable">The current <see cref="IXmlSerializable"/> implementation.</param>
         /// <returns>
         /// An XPath-navigable object.
         /// </returns>
-        public static XmlDocument Serialize(this ISerializeXml serializable)
+        public static XmlDocument Serialize(this IXmlSerializable serializable)
         {
             var document = new XmlDocument();
 
