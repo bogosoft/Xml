fc2db005d204ba3cb0efe49abc558baf557a79b9
Clayton Roth <croth0627@yahoo.com>
2017-05-30 23:53:27 -0700

    added delgate, `XmlFormatterAsync`

---
+++ Bogosoft.Xml/XmlFormatterAsync.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Bogosoft.Xml
+{
+    /// <summary>
+    /// Format an <see cref="XmlNode"/> and any of its descendants by serializing
+    /// it to a <see cref="TextWriter"/> object.
+    /// </summary>
+    /// <param name="node">
+    /// A node to serialize.
+    /// </param>
+    /// <param name="writer">
+    /// A target <see cref="TextWriter"/> object to write the node serialization to.
+    /// </param>
+    /// <param name="token">
+    /// A <see cref="CancellationToken"/> object.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Task"/> representing an asynchronous operation.
+    /// </returns>
+    public delegate Task XmlFormatterAsync(XmlNode node, TextWriter writer, CancellationToken token);
+}
