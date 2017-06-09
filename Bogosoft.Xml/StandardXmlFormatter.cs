a5c1a64e797894c439488402af07f6390788c0cc
Clayton Roth <croth0627@yahoo.com>
2017-06-08 22:14:45 -0700

    ampersands are not escaped for PCDATA

--- Bogosoft.Xml/StandardXmlFormatter.cs
+++ Bogosoft.Xml/StandardXmlFormatter.cs
@@ -40,7 +40,7 @@ namespace Bogosoft.Xml
             CancellationToken token
             )
         {
-            return writer.WriteAsync(" " + attribute.Name + "=\"" + attribute.Value + "\"", token);
+            return writer.WriteAsync(" " + attribute.Name + "=\"" + attribute.Value.Replace("&", "&amp;") + "\"", token);
         }
 
         /// <summary>
@@ -243,7 +243,7 @@ namespace Bogosoft.Xml
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         protected virtual Task FormatTextAsync(XmlText text, TextWriter writer, CancellationToken token)
         {
-            return writer.WriteAsync(text.Data, token);
+            return writer.WriteAsync(text.Data.Replace("&", "&amp;"), token);
         }
 
         /// <summary>
