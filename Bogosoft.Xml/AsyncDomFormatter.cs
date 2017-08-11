b2771b1e5eb97723c649a4779a257fb7920e60c2
Clayton Roth <croth0627@yahoo.com>
2017-08-10 22:48:34 -0700

    comment updates

--- Bogosoft.Xml/AsyncDomFormatter.cs
+++ Bogosoft.Xml/AsyncDomFormatter.cs
@@ -6,18 +6,11 @@ using System.Xml;
 namespace Bogosoft.Xml
 {
     /// <summary>
-    /// Format an <see cref="XmlNode"/> and any of its descendants by serializing
-    /// it to a <see cref="TextWriter"/> object.
+    /// Format a given DOM node to an output writer.
     /// </summary>
-    /// <param name="node">
-    /// A node to serialize.
-    /// </param>
-    /// <param name="writer">
-    /// A target <see cref="TextWriter"/> object to write the node serialization to.
-    /// </param>
-    /// <param name="token">
-    /// A <see cref="CancellationToken"/> object.
-    /// </param>
+    /// <param name="node">A DOM node to format.</param>
+    /// <param name="writer">An output writer.</param>
+    /// <param name="token">A <see cref="CancellationToken"/> object.</param>
     /// <returns>
     /// A <see cref="Task"/> representing an asynchronous operation.
     /// </returns>
