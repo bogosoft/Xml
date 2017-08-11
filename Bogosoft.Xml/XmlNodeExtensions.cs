02b7ddeaa0deaa25a0ca64d19a0fbb7a5cf1eb6c
Clayton Roth <croth0627@yahoo.com>
2017-08-10 23:52:41 -0700

    consolidated extension methods

--- Bogosoft.Xml/XmlNodeExtensions.cs
+++ Bogosoft.Xml/XmlNodeExtensions.cs
@@ -8,11 +8,10 @@ namespace Bogosoft.Xml
     public static class XmlNodeExtensions
     {
         /// <summary>
-        /// Append a child node to the current <see cref="XmlNode"/> object, returning the child as
-        /// a specific generic type.
+        /// Append a child node to the current node, returning the child as the specified DOM node type.
         /// </summary>
         /// <typeparam name="T">The type of <see cref="XmlNode"/> that will be returned.</typeparam>
-        /// <param name="parent">The current <see cref="XmlNode"/>.</param>
+        /// <param name="parent">The current node.</param>
         /// <param name="child">The child <see cref="XmlNode"/> to append to the current node.</param>
         /// <returns>The append child node as a specified type.</returns>
         public static T AppendChild<T>(this XmlNode parent, XmlNode child) where T : XmlNode
@@ -21,7 +20,74 @@ namespace Bogosoft.Xml
         }
 
         /// <summary>
-        /// Return the current <see cref="XmlNode"/> as one of its derived types.
+        /// Append a new DOM element to the current node.
+        /// </summary>
+        /// <param name="parent">The current node.</param>
+        /// <param name="name">
+        /// The qualified name of the element. If the name contains a colon then the <see cref="XmlNode.Prefix"/> 
+        /// property will reflect the part of the name preceding the colon and the
+        /// <see cref="XmlDocument.LocalName"/> property will reflect the part of the name after the colon.
+        /// The qualified name cannot include a prefix of 'xmlns'.
+        /// </param>
+        /// <returns>The newly created DOM element.</returns>
+        public static XmlElement AppendElement(this XmlNode parent, string name)
+        {
+            var child = parent.GetOwnerDocument().CreateElement(name);
+
+            parent.AppendChild(child);
+
+            return child;
+        }
+
+        /// <summary>
+        /// Append a new DOM element to the current node.
+        /// </summary>
+        /// <param name="parent">The current node.</param>
+        /// <param name="qualifiedName">
+        /// The qualified name of the element. If the name contains a colon then the <see cref="XmlNode.Prefix"/> 
+        /// property will reflect the part of the name preceding the colon and the
+        /// <see cref="XmlDocument.LocalName"/> property will reflect the part of the name after the colon.
+        /// The qualified name cannot include a prefix of 'xmlns'.
+        /// </param>
+        /// <param name="namespaceUri">The namespace URI of the element.</param>
+        /// <returns>The newly created DOM element.</returns>
+        public static XmlElement AppendElement(this XmlNode parent, string qualifiedName, string namespaceUri)
+        {
+            var child = parent.GetOwnerDocument().CreateElement(qualifiedName, namespaceUri);
+
+            parent.AppendChild(child);
+
+            return child;
+        }
+
+        /// <summary>
+        /// Append a new DOM element to the current node.
+        /// </summary>
+        /// <param name="parent">The current node.</param>
+        /// <param name="prefix">
+        /// The prefix of the new element (if any). String.Empty and null are equivalent.
+        /// </param>
+        /// <param name="localName">The local name of the new element.</param>
+        /// <param name="namespaceUri">
+        /// The namespace URI of the new element (if any). <see cref="string.Empty"/> and null are equivalent.
+        /// </param>
+        /// <returns>The newly created DOM element.</returns>
+        public static XmlElement AppendElement(
+            this XmlNode parent,
+            string prefix,
+            string localName,
+            string namespaceUri
+            )
+        {
+            var child = parent.GetOwnerDocument().CreateElement(prefix, localName, namespaceUri);
+
+            parent.AppendChild(child);
+
+            return child;
+        }
+
+        /// <summary>
+        /// Return the current node as one of its derived types.
         /// </summary>
         /// <typeparam name="T">The derived type of <see cref="XmlNode"/> to return.</typeparam>
         /// <param name="node">The current <see cref="XmlNode"/>.</param>
@@ -29,6 +95,18 @@ namespace Bogosoft.Xml
         public static T As<T>(this XmlNode node) where T : XmlNode
         {
             return node as T;
+        }
+
+        /// <summary>
+        /// Get the owning document of the current node or itself.
+        /// </summary>
+        /// <param name="node">The current <see cref="XmlNode"/> object.</param>
+        /// <returns>
+        /// The owning document of the current node or the node itself if it is a document.
+        /// </returns>
+        public static XmlDocument GetOwnerDocument(this XmlNode node)
+        {
+            return node is XmlDocument ? node as XmlDocument : node.OwnerDocument;
         }
     }
 }
