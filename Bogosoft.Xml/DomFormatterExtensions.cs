90d872f81b5d409d50855fa146724ae628cd2a57
Clayton Roth <croth0627@yahoo.com>
2017-08-10 22:22:41 -0700

    added extension methods for `AsyncDomFormatter`

--- Bogosoft.Xml/DomFormatterExtensions.cs
+++ Bogosoft.Xml/DomFormatterExtensions.cs
@@ -6,9 +6,120 @@ using System.Xml.Serialization;
 
 namespace Bogosoft.Xml
 {
-    /// <summary>Extensions for the <see cref="IDomFormatter"/> contract.</summary>
+    /// <summary>Extensions for the <see cref="AsyncDomFormatter"/> and
+    /// <see cref="IDomFormatter"/> contracts.</summary>
     public static class DomFormatterExtensions
     {
+        /// <summary>
+        /// Synchronously format a given XML-serializable object to an output writer.
+        /// </summary>
+        /// <param name="formatter">
+        /// The current <see cref="AsyncDomFormatter"/> implementation.
+        /// </param>
+        /// <param name="object">A self-serializing object to format.</param>
+        /// <param name="writer">A target to format to.</param>
+        public static void Format(this AsyncDomFormatter formatter, IXmlSerializable @object, TextWriter writer)
+        {
+            formatter.FormatAsync(@object, writer, CancellationToken.None).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Synchronously format a given XML-serializable object to an output writer.
+        /// </summary>
+        /// <param name="formatter">
+        /// The current <see cref="AsyncDomFormatter"/> implementation.
+        /// </param>
+        /// <param name="node">A DOM node to format to the output.</param>
+        /// <param name="writer">A target to format to.</param>
+        public static void Format(this AsyncDomFormatter formatter, XmlNode node, TextWriter writer)
+        {
+            formatter.Invoke(node, writer, CancellationToken.None).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Format a given XML-serializable object to an output writer.
+        /// </summary>
+        /// <param name="formatter">
+        /// The current <see cref="AsyncDomFormatter"/> implementation.
+        /// </param>
+        /// <param name="object">A self-serializing object to format.</param>
+        /// <param name="output">A target to format to.</param>
+        /// <returns>A <see cref="Task"/> representing an asynchronous operation.</returns>
+        public static Task FormatAsync(
+            this AsyncDomFormatter formatter,
+            IXmlSerializable @object,
+            TextWriter output
+            )
+        {
+            return formatter.FormatAsync(@object, output, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Format a given XML-serializable object to an output writer.
+        /// </summary>
+        /// <param name="formatter">
+        /// The current <see cref="AsyncDomFormatter"/> implementation.
+        /// </param>
+        /// <param name="node">A DOM node to format to the output.</param>
+        /// <param name="output">A target to format to.</param>
+        /// <returns>A <see cref="Task"/> representing an asynchronous operation.</returns>
+        public static Task FormatAsync(this AsyncDomFormatter formatter, XmlNode node, TextWriter output)
+        {
+            return formatter.Invoke(node, output, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Format a given XML-serializable object to an output writer.
+        /// </summary>
+        /// <param name="formatter">
+        /// The current <see cref="AsyncDomFormatter"/> implementation.
+        /// </param>
+        /// <param name="object">A self-serializing object to format.</param>
+        /// <param name="output">A target to format to.</param>
+        /// <param name="token">A <see cref="CancellationToken"/> object.</param>
+        /// <returns>A <see cref="Task"/> representing an asynchronous operation.</returns>
+        public static Task FormatAsync(
+            this AsyncDomFormatter formatter,
+            IXmlSerializable @object,
+            TextWriter output,
+            CancellationToken token
+            )
+        {
+            var document = new XmlDocument();
+
+            using (var stream = new MemoryStream())
+            using (var writer = XmlWriter.Create(stream))
+            {
+                @object.WriteXml(writer);
+
+                stream.Position = 0;
+
+                document.Load(stream);
+            }
+
+            return formatter.Invoke(document, output, token);
+        }
+
+        /// <summary>
+        /// Format a given XML-serializable object to an output writer.
+        /// </summary>
+        /// <param name="formatter">
+        /// The current <see cref="AsyncDomFormatter"/> implementation.
+        /// </param>
+        /// <param name="node">A DOM node to format to the output.</param>
+        /// <param name="output">A target to format to.</param>
+        /// <param name="token">A <see cref="CancellationToken"/> object.</param>
+        /// <returns>A <see cref="Task"/> representing an asynchronous operation.</returns>
+        public static Task FormatAsync(
+            this AsyncDomFormatter formatter,
+            XmlNode node,
+            TextWriter output,
+            CancellationToken token
+            )
+        {
+            return formatter.Invoke(node, output, token);
+        }
+
         /// <summary>Synchronously format an <see cref="XmlNode"/> to a <see cref="TextWriter"/>.</summary>
         /// <param name="formatter">The current <see cref="IDomFormatter"/> implementation.</param>
         /// <param name="node">A node to format.</param>
